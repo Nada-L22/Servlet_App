fa4b9536149981af950d49fe801d1db7373f0647
Nada <nada.layli16@gmail.com>
2024-02-19 12:18:12 +0100

    Utilisation de la Servlet

--- src/main/java/com/hatim/HelloServlet.java
+++ src/main/java/com/hatim/HelloServlet.java
@@ -1,5 +1,4 @@
-package com.hatim.app;
-
+package com.hatim;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,18 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
-
-public class AppServlet extends HttpServlet{
+public class HelloServlet extends HttpServlet{
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        out.write("<h1>Hellow Servlet with 'web.xml configuration'</h1>");
-		out.write("<h2>Enter yout name</h2>");
-		out.write("<FORM METHOD=GET ACTION='./hello-you'> ");
-		out.println("<input type='text' name='your_name'/>");
-		out.println("<input type='submit' value='ENTER'/>");
-		out.write("</FORM> ");
+		String yourName = req.getParameter("your_name"); 
+        out.write("<h1>Hi " + yourName + "!</h1>");
+
 		out.close();
     }
 
